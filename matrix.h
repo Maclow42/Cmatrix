b892ee83fd7318cd004bd01400b7f3f11df1a513
Thibault Colcomb <thibault.colcomb@hotmail.com>
2023-10-26 17:18:11 +0200

    del secured versions

--- matrix.h
+++ matrix.h
@@ -8,7 +8,6 @@
  * - The basic version returns a new matrix as the result. ex: m_func()
  * - The 'in-place' version appends 'p' at the end of the function name and modifies the imput matrix. ex: m_funcp()
  * - The 'to' version appends 't' at the end of the function name and writes the result to a provided output matrix. ex: m_funct()
- * - The 'to secure' version appends 'ts' at the end of the function name and securely allocates memory for the result. ex: m_functs()
  * 
  * Matrix Initialization:
  * - matrix_init: Initialize a matrix with random values.
@@ -18,7 +17,6 @@
  * 
  * Matrix Copy:
  * - matrix_copyto: Copy matrix to another (in-place).
- * - matrix_copytos: Copy matrix to another securely (in-place).
  * - matrix_getcpy: Return a deep copy of the matrix.
  * 
  * Matrix Operations:
@@ -63,9 +61,8 @@ matrix_t* matrix_nId(size_t dim, MATRIX_TYPE n);
 MATRIX_TYPE matrix_get(const matrix_t *m1, size_t i, size_t j);
 void matrix_set(matrix_t *m1, size_t i, size_t j, MATRIX_TYPE val);
 
-// deep copy of matrix (secure and unsecure)
-void matrix_copyto(matrix_t* dest, const matrix_t* src);
-void matrix_copytos(matrix_t* dest, const matrix_t* src);
+// deep copy of matrix
+void matrix_copyto(const matrix_t* src, matrix_t* dest);
 // return a deep copy of matrix
 matrix_t* matrix_getcpy(const matrix_t* src);
 
@@ -76,69 +73,57 @@ void matrix_free(matrix_t *m1);
 matrix_t *m_add(const matrix_t *m1, const matrix_t *m2);
 void m_addp(matrix_t *m1, const matrix_t *m2);
 void m_addt(const matrix_t *m1, const matrix_t *m2, matrix_t *result);
-void m_addts(const matrix_t *m1, const matrix_t *m2, matrix_t *result);
 
 // matrix scalar addition
 matrix_t* m_kadd(const matrix_t *m1, MATRIX_TYPE k);
 void m_kaddp(matrix_t *m1, MATRIX_TYPE k);
 void m_kaddt(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
-void m_kaddts(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
 
 // matrix subtraction
 matrix_t *m_sub(const matrix_t *m1, const matrix_t *m2);
 void m_subp(matrix_t *m1, const matrix_t *m2);
 void m_subt(const matrix_t *m1, const matrix_t *m2, matrix_t *result);
-void m_subts(const matrix_t *m1, const matrix_t *m2, matrix_t *result);
 
 // matrix scalar subtraction
 matrix_t* m_ksub(const matrix_t *m1, MATRIX_TYPE k);
 void m_ksubp(matrix_t *m1, MATRIX_TYPE k);
 void m_ksubt(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
-void m_ksubts(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
 
 // matrix multiplication
 matrix_t* m_mul(const matrix_t *m1, const matrix_t *m2);
 void m_mult(const matrix_t *m1, const matrix_t *m2, matrix_t *result);
-void m_mults(const matrix_t *m1, const matrix_t *m2, matrix_t *result);
 
 // matrix power
 matrix_t* m_pow(const matrix_t *m1, size_t n);
 void m_powp(matrix_t *m1, size_t n);
 void m_powt(const matrix_t *m1, size_t n, matrix_t *result);
-void m_powts(const matrix_t *m1, size_t n, matrix_t *result);
 
 // matrix scalar multiplication
 matrix_t* m_kmul(const matrix_t *m1, MATRIX_TYPE k);
 void m_kmulp(matrix_t *m1, MATRIX_TYPE k);
 void m_kmult(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
-void m_kmults(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
 
 // matrix scalar division
 matrix_t* m_kdiv(const matrix_t *m1, MATRIX_TYPE k);
 void m_kdivp(matrix_t *m1, MATRIX_TYPE k);
 void m_kdivt(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
-void m_kdivts(const matrix_t *m1, MATRIX_TYPE k, matrix_t *result);
 
 // matrix scalar product
-MATRIX_TYPE *m_dot(const matrix_t *m1, const matrix_t *m2);
+MATRIX_TYPE m_dot(const matrix_t *m1, const matrix_t *m2);
 
 // matrix transposition
 matrix_t *m_transp(const matrix_t *m1);
 void m_transpt(const matrix_t *m1, matrix_t *result);
-void m_transpts(const matrix_t *m1, matrix_t *result);
 
 // matrix sum following a direction
 matrix_t *m_sumfd(const matrix_t *m1, matrix_dir_t dir);
 void m_sumfdt(const matrix_t *m1, matrix_t *result, matrix_dir_t dir);
-void m_sumfdts(const matrix_t *m1, matrix_t *result, matrix_dir_t dir);
 
 // matrix multiplication following a direction
 matrix_t *m_mulfd(const matrix_t *m1, matrix_dir_t dir);
 void m_mulfdt(const matrix_t *m1, matrix_t *result, matrix_dir_t dir);
-void m_mulfdts(const matrix_t *m1, matrix_t *result, matrix_dir_t dir);
 
 // matrix apply function to all elements
 matrix_t *m_apply(const matrix_t *m1, MATRIX_TYPE (*f)(MATRIX_TYPE));
 void m_applyp(const matrix_t *m1, MATRIX_TYPE (*f)(MATRIX_TYPE));
 void m_applyt(const matrix_t *m1, matrix_t *result, MATRIX_TYPE (*f)(MATRIX_TYPE));
-void m_applyts(const matrix_t *m1, matrix_t *result, MATRIX_TYPE (*f)(MATRIX_TYPE));
